76f76a66e076f5def5b371190a4f41162178abe3
Darlelet <aurelien.darragon@gmail.com>
2016-07-02 15:50:38 +0200

    minor fixes + semantic minifying

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php
   echo '<button class="fluid ui inverted red button">' .
   posix_getpwuid(posix_geteuid())['name'] . '@' .
-  gethostname() . "_-_" . '<span id="time" style="position: fixed;"></span></button>';
+  gethostname() . "_-_" . '<span id="time" style="position: absolute;"></span></button>';
 ?>
